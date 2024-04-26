3fa5a832743752de6ac38570d57ba6e6ac2a5054
FeorianiN <fioristfearlesss@gmail.com>
2024-04-26 18:36:38 +0400

    Refactoring GetScore()
    [REF] The pointNames dictionary is used to compare numeric values with the names of points and the calculation logic is simplified and shortened

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System;
+
 namespace Tennis
 {
     public class TennisGame2 (string player1Nam, string player2Nam) : ITennisGame
@@ -11,90 +14,41 @@ namespace Tennis
 
         public TennisGame2() : this (player1Name, player2Name) { }
 
+        // determining the current score in a game
         public string GetScore()
         {
             var score = "";
-            if (p1point == p2point && p1point < 3)
+            var pointNames = new Dictionary<int, string>
             {
-                if (p1point == 0)
-                    score = "Love";
-                if (p1point == 1)
-                    score = "Fifteen";
-                if (p1point == 2)
-                    score = "Thirty";
-                score += "-All";
-            }
-            if (p1point == p2point && p1point > 2)
-                score = "Deuce";
+                { 0, "Love" },
+                { 1, "Fifteen" },
+                { 2, "Thirty" },
+                { 3, "Forty" }
+            };
 
-            if (p1point > 0 && p2point == 0)
+            // processing of tied results
+            if (p1point == p2point)
             {
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
+                if (p1point < 3)
+                    score = pointNames[p1point] + "-All";
+                else
+                    score = "Deuce";
+            }
+            else if (p1point >= 4 || p2point >= 4)
+            {
+                var pointDiff = Math.Abs(p1point - p2point);
 
-                p2res = "Love";
-                score = p1res + "-" + p2res;
-            }
-            if (p2point > 0 && p1point == 0)
-            {
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
+                // if the gap is big enough, the game ends
+                if (pointDiff >= 2)
+                    score = "Win for " + (p1point > p2point ? "player1" : "player2");
 
-                p1res = "Love";
-                score = p1res + "-" + p2res;
+                // processing advantage game-stage
+                else if (pointDiff == 1)
+                    score = "Advantage " + (p1point > p2point ? "player1" : "player2");
             }
+            else
+                score = pointNames[p1point] + "-" + pointNames[p2point];
 
-            if (p1point > p2point && p1point < 4)
-            {
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                score = p1res + "-" + p2res;
-            }
-            if (p2point > p1point && p2point < 4)
-            {
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                score = p1res + "-" + p2res;
-            }
-
-            if (p1point > p2point && p2point >= 3)
-            {
-                score = "Advantage player1";
-            }
-
-            if (p2point > p1point && p1point >= 3)
-            {
-                score = "Advantage player2";
-            }
-
-            if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
-            {
-                score = "Win for player1";
-            }
-            if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
-            {
-                score = "Win for player2";
-            }
             return score;
         }
 
